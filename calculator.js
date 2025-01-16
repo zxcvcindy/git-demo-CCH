f43b6df94553ddf3eab60cce6ec28e25da08032f
Josh Hsieh <josh.hsieh@linux.com>
2025-01-16 14:15:35 +0800

    chore: init commit

---
+++ calculator.js
@@ -0,0 +1,17 @@
+class calculator {
+	add(x, y) {
+		// TODO: implement add method
+	}
+
+	subtract(x, y) {
+		// TODO: implement subtract method
+	}
+
+	multiply(x, y) {
+		// TODO: implement multiply method
+	}
+
+	divide(x, y) {
+		//TODO: implement divide method
+	}
+}
